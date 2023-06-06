b010be60d52812cfbfbc22c40acb8caa1d2a2abc
Ali <caitlinaliflynn@gmail.com>
2023-06-05 19:41:08 -0500

    finished number 1. of Controllers portion of Part 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotEmpty
+    @Size(min = 1, max = 100)
+    private String location;
 
+    public Employer() {
+
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
